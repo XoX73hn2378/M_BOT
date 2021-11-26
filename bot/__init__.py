f57d648ff019a11ba26d6fadc00d2bf922d927c9
ARCHRootBoot <90963763+archrootboot@users.noreply.github.com>
2021-11-26 13:45:39 +0530

    Update __init__.py

--- bot/__init__.py
+++ bot/__init__.py
@@ -290,9 +290,9 @@ except KeyError:
 try:
     IMAGE_URL = getConfig('IMAGE_URL')
     if len(IMAGE_URL) == 0:
-        IMAGE_URL = 'https://telegra.ph/file/019996f816db9ed576cff.jpg'
+        IMAGE_URL = 'https://telegra.ph/file/b5b85a060b63cc6572810.jpg'
 except KeyError:
-    IMAGE_URL = 'https://telegra.ph/file/019996f816db9ed576cff.jpg'
+    IMAGE_URL = 'https://telegra.ph/file/b5b85a060b63cc6572810.jpg'
 try:
     STOP_DUPLICATE = getConfig('STOP_DUPLICATE')
     if STOP_DUPLICATE.lower() == 'true':
